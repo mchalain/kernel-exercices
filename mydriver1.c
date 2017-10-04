9c684cbf083b8d1651b659afabf9bbe3e49d3d84
Marc Chalain <marc.chalain@smile.fr>
2017-10-04 13:03:37 +0200

    [char driver] create "mydriver class"

--- mydriver1.c
+++ mydriver1.c
@@ -59,10 +59,14 @@ static struct file_operations my_fops = {
 };
 
 static struct cdev my_cdev;
+static struct class *my_class = NULL;
 
 static int __init my_init(void)
 {
 	int ret;
+	struct device *device = NULL;
+
+	my_class = class_create(THIS_MODULE, "mydrivers");
 
 	cdev_init(&my_cdev, &my_fops);
 	my_cdev.owner = THIS_MODULE;
@@ -70,15 +74,20 @@ static int __init my_init(void)
 	ret = cdev_add(&my_cdev, MKDEV(MY_MAJOR, 0), 1);
 	if (ret) panic("Couldn't register /dev/mydriver driver\n"); 
 
-	printk(KERN_INFO "my char driver: create special file\n");
-	printk(KERN_INFO "\tmknod /dev/mydriver0 c 60 0\n");
+	ret = register_chrdev_region(MKDEV(MY_MAJOR, 0), 1, "/dev/mydriver");
+	if (ret < 0) panic("Couldn't register /dev/tty driver\n"); 
 
+	device = device_create(my_class, NULL, MKDEV(MY_MAJOR, 0), NULL, "mydriver");
+	
 	return 0;
 }
 
 static void __exit my_exit(void)
 {
+  device_destroy(my_class, MKDEV(MY_MAJOR, 0));
   cdev_del(&my_cdev);
+  class_destroy(my_class);
+	unregister_chrdev_region(MKDEV(MY_MAJOR, 0), 1);
 }
 
 /*
