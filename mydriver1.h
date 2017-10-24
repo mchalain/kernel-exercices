717ee913c72e5c8529ae9866232700c8779d5248
Marc Chalain <marc.chalain@smile.fr>
2017-10-24 11:35:44 +0200

    [module] initialization and release modules functions

--- mydriver1.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MYDRIVER1_H
-#define _MYDRIVER1_H
-
-#define RPI_GPIO_IOC_MAGIC	'k'
-#define RPI_GPIO_SET		_IO(RPI_GPIO_IOC_MAGIC, 128)
-#define RPI_GPIO_CLEAR		_IO(RPI_GPIO_IOC_MAGIC, 129)
-
-#endif
