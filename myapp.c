04bb80a054bf77168b415d04324b4e2276d9a913
Marc Chalain <marc.chalain@smile.fr>
2017-10-31 11:19:49 +0100

    add myapp to test

---
+++ myapp.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include "mydriver1.h"
+
+int main(int argc, char **argv)
+{
+	char *device = NULL;
+	int gpio_nr = 0;
+
+	int opt;
+	do
+	{
+		opt = getopt(argc, argv, "hd:");
+		switch (opt)
+		{
+			case 'h':
+				printf("%s -d <device path> -n <gpio number>", argv[0]);
+				exit(-1);
+			break;
+			case 'd':
+				device = optarg;
+			break;
+			case 'n':
+				gpio_nr = atoi(optarg);
+			break;
+		}
+	} while(opt != -1);
+
+	if (device == NULL)
+		return -1;
+	int fd = open(device, O_RDWR);
+	if (fd > 0)
+	{
+		int ret;
+		int run = 1;
+		while (run)
+		{
+			int c = getchar();
+			switch (c)
+			{
+				case 'q':
+					run = 0;
+				break;
+				case '1':
+					ret = ioctl(fd, RPI_GPIO_SET, (void *)gpio_nr);
+				break;
+				case '0':
+					ret = ioctl(fd, RPI_GPIO_CLEAR, (void *)gpio_nr);
+				break;
+			}
+		}
+		close(fd);
+	}
+	return 0;
+}
