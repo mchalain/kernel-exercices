131fa9598986d65f35fb580b1dbc52f8b6ab09df
Marc Chalain <marc.chalain@smile.fr>
2017-10-31 11:27:30 +0100

    README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+module.1 :
+--------
+Création d'un module simple avec initialisation et déchargement du module.
+
+module.2 :
+--------
--- mydriver1.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MYDRIVER1_H
-#define _MYDRIVER1_H
-
-#define RPI_GPIO_IOC_MAGIC	'k'
-#define RPI_GPIO_SET		_IOW(RPI_GPIO_IOC_MAGIC, 128, short)
-#define RPI_GPIO_CLEAR		_IOW(RPI_GPIO_IOC_MAGIC, 129, short)
-#define RPI_GPIO_GET		_IOR(RPI_GPIO_IOC_MAGIC, 130, uint32_t)
-
-#endif
