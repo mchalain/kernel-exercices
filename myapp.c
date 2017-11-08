72e2d34d7a750a9995d7fbaff654986097f8e5e1
mchalain <marc.chalain@smile.fr>
2017-11-08 12:25:39 +0000

    myapp uses /sys/module/mydriver1/parameters/gpio_nr

--- myapp.c
+++ myapp.c
@@ -5,18 +5,19 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdint.h>
 
 #include "mydriver1.h"
 
 int main(int argc, char **argv)
 {
 	char *device = NULL;
-	int gpio_nr = 0;
+	char *gpio_nr = NULL;
 
 	int opt;
 	do
 	{
-		opt = getopt(argc, argv, "hd:");
+		opt = getopt(argc, argv, "hd:n:");
 		switch (opt)
 		{
 			case 'h':
@@ -27,13 +28,22 @@ int main(int argc, char **argv)
 				device = optarg;
 			break;
 			case 'n':
-				gpio_nr = atoi(optarg);
+				gpio_nr = optarg;
 			break;
 		}
 	} while(opt != -1);
 
 	if (device == NULL)
 		return -1;
+	if (gpio_nr != NULL)
+	{
+		int fd = open("/sys/module/mydriver1/parameters/gpio_nr", O_RDWR);
+		if (fd > 0)
+		{
+			write(fd, gpio_nr, 2);
+			close(fd);
+		}
+	}
 	int fd = open(device, O_RDWR);
 	if (fd > 0)
 	{
@@ -48,10 +58,17 @@ int main(int argc, char **argv)
 					run = 0;
 				break;
 				case '1':
-					ret = ioctl(fd, RPI_GPIO_SET, (void *)gpio_nr);
+					ret = ioctl(fd, RPI_GPIO_SET);
 				break;
 				case '0':
-					ret = ioctl(fd, RPI_GPIO_CLEAR, (void *)gpio_nr);
+					ret = ioctl(fd, RPI_GPIO_CLEAR);
+				break;
+				case 'r':
+				{
+					uint32_t value;
+					ret = ioctl(fd, RPI_GPIO_GET, &value);
+					printf("gpio value %ld\n", value);
+				}
 				break;
 			}
 		}
