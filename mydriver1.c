d3931b01c576ba6067ac183cbd3085b6b7d33654
mchalain <marc.chalain@smile.fr>
2017-11-08 13:35:53 +0000

    la fonction read sort une chaine de caractères "Bonjour"

--- mydriver1.c
+++ mydriver1.c
@@ -6,6 +6,8 @@
 #include <linux/fs.h>           /* file_operations */
 #include <linux/cdev.h>
 #include <linux/device.h>
+#include <linux/vmalloc.h>
+#include <linux/uaccess.h>
 
 #include <linux/miscdevice.h>
 
@@ -16,16 +18,24 @@ MODULE_LICENSE("GPL");
 /*
  * Arguments
  */
-
+struct my_data_t
+{
+	int read;
+};
 /*
  * File operations
  */
 static ssize_t my_read(struct file *file, char *buf, size_t count, loff_t *ppos)
 {
+	struct my_data_t *data = (struct my_data_t *)file->private_data;
 	printk(KERN_INFO "my char driver: read()\n");
 
-	count = 0;
+	if (data->read == 8)
+		return 0;
+	copy_to_user(buf, "Bonjour\n", 8);
+	count = 8;
 	*ppos += count;
+	data->read = count;
 	return count;
 }
 
@@ -39,14 +49,17 @@ static ssize_t my_write(struct file *file, const char *buf, size_t count, loff_t
 
 static int my_open(struct inode *inode, struct file *file)
 {
+	struct my_data_t *data = vmalloc(sizeof(*data));
+	data->read = 0;
 	printk(KERN_INFO "my char driver: open()\n");
-
+	file->private_data = data;
 	return 0;
 }
 
 static int my_release(struct inode *inode, struct file *file)
 {
 	printk(KERN_INFO "my char driver: release()\n");
+	vfree(file->private_data);
 
 	return 0;
 }
