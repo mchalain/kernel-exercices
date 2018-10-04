04484ca752ae4661ae3589e18e45b27c994fe9c5
Marc Chalain <marc.chalain@smile.fr>
2018-10-04 15:06:01 +0200

    add new parameter

--- mydriver1.c
+++ mydriver1.c
@@ -8,13 +8,17 @@ MODULE_DESCRIPTION("mydriver1");
 MODULE_AUTHOR("Marc Chalain, Smile ECS");
 MODULE_LICENSE("GPL");
 
+static char my_str[256] = "Enter in a function of mydriver1";
+module_param_string(message, my_str, sizeof(my_str), 0644);
+MODULE_PARM_DESC(message,"Une chaîne de caractères");
+
 /*
  * Arguments
  */
 
 int mydriver1_print(void)
 {
-	printk(KERN_INFO"Enter in a function of mydriver1\n");
+	printk(KERN_INFO"%s\n", my_str);
 	return 0;
 }
 EXPORT_SYMBOL(mydriver1_print);
