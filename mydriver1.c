2db5da03a1aae9863e99f2a71a9d271db1be9e39
Marc Chalain <marc.chalain@smile.fr>
2018-10-22 16:59:33 +0200

    first release

--- mydriver1.c
+++ mydriver1.c
@@ -7,9 +7,11 @@
 #include <linux/cdev.h>
 #include <linux/device.h>
 #include <linux/vmalloc.h>
+#include <linux/slab.h>
 #include <linux/uaccess.h>
 
 #include <linux/miscdevice.h>
+#include <linux/i2c.h>
 
 MODULE_DESCRIPTION("mydriver1");
 MODULE_AUTHOR("Marc Chalain, Smile ECS");
@@ -20,6 +22,8 @@ MODULE_LICENSE("GPL");
  */
 struct my_data_t
 {
+	struct miscdevice misc;
+	struct i2c_client *client;
 	int read;
 };
 /*
@@ -27,15 +31,13 @@ struct my_data_t
  */
 static ssize_t my_read(struct file *file, char *buf, size_t count, loff_t *ppos)
 {
-	struct my_data_t *data = (struct my_data_t *)file->private_data;
 	printk(KERN_INFO "my char driver: read()\n");
 
-	if (data->read == 8)
+	if (*ppos >= 8)
 		return 0;
 	copy_to_user(buf, "Bonjour\n", 8);
 	count = 8;
 	*ppos += count;
-	data->read = count;
 	return count;
 }
 
@@ -49,10 +51,7 @@ static ssize_t my_write(struct file *file, const char *buf, size_t count, loff_t
 
 static int my_open(struct inode *inode, struct file *file)
 {
-	struct my_data_t *data = vmalloc(sizeof(*data));
-	data->read = 0;
 	printk(KERN_INFO "my char driver: open()\n");
-	file->private_data = data;
 	return 0;
 }
 
@@ -74,19 +73,73 @@ static struct file_operations my_fops = {
 
 static struct miscdevice mymisc;
 
+/**
+ * I2C functions
+ */
+/**
+ * method called by the probe of the I2C bus
+ */
+static int
+my_probe(struct i2c_client *client, const struct i2c_device_id *id)
+{
+	struct my_data_t *data;
+	pr_info("device %X\n", client->addr);
+
+	data = kzalloc(sizeof(*data), GFP_KERNEL);
+	data->client = client;
+	i2c_set_clientdata(client, data);
+
+	data->misc.minor = MISC_DYNAMIC_MINOR;
+	data->misc.name = "mydriver";
+	data->misc.fops = &my_fops;
+
+	misc_register(&data->misc);
+
+	return 0;
+}
+
+static int
+my_remove(struct i2c_client *client)
+{
+	struct my_data_t *data = i2c_get_clientdata(client);
+	misc_deregister(&data->misc);
+	kfree(data);
+	return 0;
+}
+
+static const struct i2c_device_id my_ids[] = {
+	{ "myi2c", 0, },
+};
+
+struct i2c_board_info my_boardinfo =
+{
+	I2C_BOARD_INFO("myi2c", 0x42),
+};
+
+static struct i2c_driver my_i2c = {
+	.driver = {
+		.name	= "mydriver",
+	},
+	.probe		= my_probe,
+	.remove		= my_remove,
+	.id_table	= my_ids,
+};
+
 static int __init my_init(void)
 {
-	mymisc.minor = MISC_DYNAMIC_MINOR;
-	mymisc.name = "mydriver";
-	mymisc.fops = &my_fops;
+	struct i2c_adapter *adapter;
 
-	misc_register(&mymisc);
+	i2c_register_driver(THIS_MODULE, &my_i2c);
+
+	adapter = i2c_get_adapter(1);
+	i2c_new_device(adapter, &my_boardinfo);
+
 	return 0;
 }
 
 static void __exit my_exit(void)
 {
-	misc_deregister(&mymisc);
+	i2c_del_driver(&my_i2c);
 }
 
 /*
