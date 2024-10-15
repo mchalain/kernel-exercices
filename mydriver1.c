ad88e4aae24656e9c785874c3dd9a9dbff0cd019
Marc Chalain <marc.chalain@gmail.com>
2024-10-15 06:57:25 +0200

    replace devm_kzalloc by kzalloc

--- mydriver1.c
+++ mydriver1.c
@@ -140,7 +140,7 @@ static int my_probe(struct platform_device *dev)
 	struct mydriver1_data_t *ddata = (struct mydriver1_data_t *)dev_get_platdata(&dev->dev);
 	if (ddata == NULL)
 	{
-		ddata = devm_kzalloc(&dev->dev, sizeof(*ddata), GFP_KERNEL);
+		ddata = kzalloc(sizeof(*ddata), GFP_KERNEL);
 		ddata->pdev = dev;
 	}
 	pr_info("probe ddata %p\n",ddata);
@@ -166,6 +166,7 @@ static int my_remove(struct platform_device *dev)
 	if (ddata)
 	{
 		misc_deregister(&ddata->misc);
+		kvfree(ddata);
 	}
 	return 0;
 }
