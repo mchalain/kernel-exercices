48360e8cbd0d4760ca3cd644bb3c7779fa3d9e54
Marc Chalain <marc.chalain@gmail.com>
2024-10-21 18:13:27 +0200

    use regmap to access to the registers

--- mydriver1.c
+++ mydriver1.c
@@ -13,7 +13,7 @@
 #include <linux/platform_device.h>
 #include <linux/of.h>
 
-#include <linux/io.h>
+#include <linux/regmap.h>
 
 #include "mydriver1.h"
 
@@ -39,8 +39,8 @@ static ssize_t my_read(struct file *file, char *buf, size_t count, loff_t *ppos)
 	struct mydriver1_data_t *ddata = (struct mydriver1_data_t *)file->private_data;
 	dev_info(&ddata->pdev->dev, "read()\n");
 	int length = 0;
-	//uint32_t value = *((uint32_t*)ddata->memregion.mem + offset);
-	uint32_t value = readl(ddata->memregion.mem + offset);
+	uint32_t value;
+	regmap_read(ddata->regmap, offset, &value);
 	length = snprintf(buf, count, "reg %#lX %#X\n", ddata->memregion.start + offset, value);
 	if (ddata->read >= length)
 		return 0;
@@ -69,7 +69,7 @@ static ssize_t my_write(struct file *file, const char *buf, size_t count, loff_t
 			value <<= 4;
 		}
 		dev_info(&ddata->pdev->dev, "write(%#lX, %#X)\n", offset, value);
-		 *((uint32_t*)ddata->memregion.mem + offset) = value;
+		regmap_write(ddata->regmap, offset, value);
 	}
 	*ppos += count;
 	return count;
@@ -90,8 +90,6 @@ static int my_open(struct inode *inode, struct file *file)
 	{
 		ddata->read = 0;
 		file->private_data = ddata;
-		if (ddata->memregion.mem == NULL)
-			ddata->memregion.mem = ioremap(ddata->memregion.start, ddata->memregion.end - ddata->memregion.start);
 		printk(KERN_INFO "my char driver: open()\n");
 		return 0;
 	}
@@ -102,7 +100,6 @@ static int my_release(struct inode *inode, struct file *file)
 {
 	struct mydriver1_data_t *ddata = (struct mydriver1_data_t *)file->private_data;
 	dev_info(&ddata->pdev->dev, "release()\n");
-	iounmap(ddata->memregion.mem);
 	return 0;
 }
 
@@ -112,6 +109,13 @@ static struct file_operations my_fops = {
 	.write =	my_write,
 	.open =		my_open,
 	.release =	my_release,
+};
+
+static const struct regmap_config myregmap_config =
+{
+	.reg_bits = 32,
+	.reg_stride = 4,
+	.val_bits = 32,
 };
 
 static int my_probe(struct platform_device *pdev)
@@ -135,6 +139,10 @@ static int my_probe(struct platform_device *pdev)
 		dev_set_drvdata(&pdev->dev, ddata);
 		ddata->misc = misc;
 		list_add(&ddata->list, &mydevice_list);
+
+		void __iomem *registers = devm_platform_ioremap_resource(pdev, 0);
+		if (registers)
+			ddata->regmap = regmap_init_mmio(&pdev->dev, registers, &myregmap_config);
 	}
 	const char *string = NULL;
 	struct device_node *node = ddata->pdev->dev.of_node;
@@ -147,25 +155,6 @@ static int my_probe(struct platform_device *pdev)
 	{
 		dev_info(&pdev->dev, "access to %s\n", string);
 	}
-	int i;
-	struct resource * res;
-	dev_info(&pdev->dev, "nb resources %d\n", pdev->num_resources);
-
-	for (i = 0; i < pdev->num_resources; i++)
-	{
-		//ddata->memregion.mem = devm_platform_get_and_ioremap_resource(dev, i, &res);
-		// or
-		res = platform_get_resource(pdev, IORESOURCE_MEM, i);
-		if (res && ddata)
-		{
-			ddata->memregion.start = res->start;
-			ddata->memregion.end = res->end;
-			dev_info(&pdev->dev, "reg %d : 0x%lX 0x%lX\n", i, ddata->memregion.start, ddata->memregion.end);
-			//ddata->memregion.mem = devm_ioremap_resource(&dev->dev, res);
-			// or
-			//ddata->memregion.mem = devm_platform_ioremap_resource(dev, res);
-		}
-	}
 
 	return 0;
 }
@@ -177,6 +166,8 @@ static int my_remove(struct platform_device *pdev)
 	{
 		misc_deregister(ddata->misc);
 	}
+	if (ddata->regmap)
+		regmap_exit(ddata->regmap);
 	return 0;
 }
 
